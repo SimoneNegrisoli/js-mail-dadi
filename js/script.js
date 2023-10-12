f9896de46dca00c5bbbb04dcf59f2c8c13807963
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-12 19:28:37 +0200

    esercizio dadi

--- js/script.js
+++ js/script.js
@@ -8,11 +8,16 @@
 
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 // Consigli del giorno:
+
+
+
 // 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
 // 3. si ma noi cosa vogliamo fare?
@@ -25,41 +30,7 @@
 //   2.   Grafica per il gioco dei dadi
 
 
-
-// Esercizio Email
-
-    // const dateEmail = document.getElementById('email');
-    // console.log(dateEmail)
-
-    // const authorizedEmailAddresses = [
-    //     'prima@gmail.com',
-    //     'seconda@gmail.com',
-    //     'terza@gmail.com',
-    //     'quarta@gmail.com',
-    //     'quinta@gmail.com',
-    //     'sesta@gmail.com',
-    //     'settima@gmail.com'  
-    // ]
- 
-    // const btnEmail = document.querySelector('button');
-    // const autorizzato = document.getElementById('autorizzato')
-    // const nonautorizzato = document.getElementById('nonautorizzato')
-
-    // btnEmail.addEventListener('click', function(){
-    //     // mail inserita dall' utente 
-
-    //     const userEnterEmail = dateEmail.value;
-    //     console.log(userEnterEmail)
-        
-    //     if (authorizedEmailAddresses.includes(userEnterEmail)){
-    //         autorizzato.classList.remove('d-none');
-    //         nonautorizzato.classList.add('d-none');
-    //     } else {
-    //         nonautorizzato.classList.remove('d-none');
-    //         autorizzato.classList.add('d-none');
-    //     }
-    // }
-    // )
+// Esercio Email
 
     const dateEmail = document.getElementById('email');
 
@@ -95,29 +66,26 @@
 
     })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // Esercizio Dadi
 
-//     const dicegame = document.getElementById('dicegame')
+    // utility
+    function getRndInteg (min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-//     const numbers[]
+
+    const dicegameEl = document.getElementById('dicegame')
+    const number = [1, 2, 3, 4, 5, 6]
+
+    let userNumber = getRndInteg(0 , number.length -1);
+    let computerNumber = getRndInteg(0 , number.length -1);
+    number[userNumber]
+    number[computerNumber]
+    
+    console.log(number[computerNumber])
+    console.log(number[userNumber])
+
+    for(x = 0; x < number.length; x++){
+        // console.log(number[x])
+    }
+
